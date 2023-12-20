fd174c4a0cd2dbe974e9671d540950ac33547660
Fetana <fetanak@gmail.com>
2023-12-20 20:47:17 +0200

    js, hw 6, try 1

---
+++ js/task-2.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+class Storage {
+    #items;
+
+
+    constructor(items = []){
+        this.#items = items;
+    }
+    getItems(){
+        return this.#items;
+    }
+    addItem(newItem){
+        this.#items.push(newItem);
+    }
+    removeItem(itemToRemove){
+        if(this.#items.includes(itemToRemove)){
+            this.#items.splice(this.#items.indexOf(itemToRemove) - 1, 1);
+        }
+
+    }
+  removeItem(itemToRemove){
+   if(this.#items.includes(itemToRemove)){
+        this.#items.splice(this.#items.indexOf(itemToRemove) - 1, 1);
+    }
+
+}
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+
